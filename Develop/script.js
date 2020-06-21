38c6e5ae4547bebde888008e26cbd82a7f3b8587
sitakura <scot.itakura@gmail.com>
2020-06-20 17:18:25 -0700

    cleaned up code, commented js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,31 @@
-// GIVEN I am using a daily planner to create a schedule
-
-    // WHEN I open the planner
-    // THEN the current day is displayed at the top of the calendar
+// Display date from moment.js
 var currentDate = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(currentDate);
 
-    // WHEN I scroll down
-    // THEN I am presented with time blocks for standard business hours
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// For loop for every hour and if statement to display background color depending on the time of day
 var timeCheck = function() {
     for (var i = 9; i < 18; i++) {
         var stringI = i.toString();
         if (i < moment().format("H")) {
             $("#" + stringI).addClass("past");
-            $("#" + stringI).removeClass("future");
             $("#" + stringI).removeClass("present");
+            $("#" + stringI).removeClass("future");
         }
         else if (i > moment().format("H")) {
             $("#" + stringI).removeClass("past");
-            $("#" + stringI).addClass("future");
             $("#" + stringI).removeClass("present");
+            $("#" + stringI).addClass("future");
         }
         else {
             $("#" + stringI).removeClass("past");
-            $("#" + stringI).removeClass("future");
             $("#" + stringI).addClass("present");
+            $("#" + stringI).removeClass("future");
         }
     }
 };
 timeCheck();
-console.log(moment().format("H"));
 
-    // WHEN I click into a time block
-    // THEN I can enter an event
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
+// Make button save to local storage
 $(".saveBtn").click(function() {
     var input = $(this).siblings(".description");
     var inputValue = input.val()
@@ -46,9 +34,7 @@ $(".saveBtn").click(function() {
     localStorage.setItem(hour, inputValue);
 });
 
-// WHEN I refresh the page
-// THEN the saved events persist
-// Loop through hours 
+// Loop through hours to get from local storage
 var updateTasks = function() {
     for (var i = 9; i < 18; i++) {
         var stringI = i.toString();
